2ab6492a033293ff8193b67129b1be5d8d7b2cf3
ThomasMeulendijks <t.meulendijks@outlook.com>
2017-06-07 13:07:34 +0200

    First upload

---
+++ HotelV1.2/MainWindow.xaml.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Hotel;
+
+namespace HotelV1._2
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public List<Room> Rooms;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            Rooms = GetRooms();
+
+        }
+
+        private List<Room> GetRooms()
+        {
+            List<Room> Rooms = new List<Room>();
+            for (int i = 0; i < 20; i++)
+            {
+                int roomNr = i;
+                int size;
+                if (i % 3 == 0)
+                {
+                    size = 2;
+                }
+                else
+                {
+                    size = 4;
+                }
+                Room newRoom = new Room(roomNr, size);
+                Rooms.Add(newRoom);
+            }
+
+            return Rooms;
+        }
+
+        private void btnNewReservation_Click(object sender, RoutedEventArgs e)
+        {
+            ReservationWindow newReservationWindow = new ReservationWindow(Rooms);
+            newReservationWindow.ShowDialog();
+            if (newReservationWindow.DialogResult.Value)        
+            {
+                
+            }
+        }
+    }
+}
